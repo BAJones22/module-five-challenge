b48322d453effbb0fc955e205782953565198d90
Brooke Adlesperger-Jones <badles@live.com>
2023-04-20 21:24:55 -0500

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # module-five-challenge
 Work Day Scheduler
+
+## Description
+
+The goal of this project was to create a functional work day scheduler. 
+
+## Installation
+
+N/A
+
+## Usage
+
+<img src="./assets/images/RM1.png"/>
+<img src="./assets/images/RM2.png"/>
+
+
+The usage of this work day scheduler is a simple web application where a user has the ability to type plans for their workday and hit the save button to save those plans into the local storage. The work day is broken into 9 hour blocks that are color coordinated to represent time that is past, present and future. The webpage automatically refreshes the time every minute to change the colors accordingly. 
+
+## Credits
+
+The original code originated from the following link: src="https://github.com/coding-boot-camp/crispy-octo-meme"
+
+Referenced the following link for local storage: src="https://stackoverflow.com/questions/73565932/i-have-added-a-local-storage-function-but-it-doesnt-seem-to-work-on-my-scheduler"
+
+## License
+
+Please refer to the license in the repo
--- assets/script.js
+++ assets/script.js
@@ -1,46 +1,45 @@
 $(function () {
   // Set current date/time in military format
-var currentHour = dayjs().format('HH');
-$("#currentDay").text(dayjs().format('dddd, MMMM D, YYYY'));
+  var currentHour = dayjs().format("HH");
+  $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY"));
 
-// Set time blocks to corresponding color based on current time
-function updateColor() {
-  $('.time-block').each(function() {
-    var timeHour = parseInt(this.id);
-    console.log(timeHour);
-    console.log(this);
-    if (timeHour === currentHour) {
-      $(this).toggleClass('present');
-    } else if (timeHour < currentHour) {
-      $(this).toggleClass('past');
-    } else {
-      $(this).toggleClass('future');
-    }
-  });
-}
-// Update time on page every minute
-setInterval(function() {
-  updateColor();
-}, 60000);
-
-// Grab input from text area and save to local storage when clicking save button 
-function textInput() {
-  $('.saveBtn').on('click', function() {
-    var key = $(this).parent().attr('id');
-    var value = $(this).siblings('.description').val();
-    localStorage.setItem(key, value);
-  });
-}
-
-// Return the information from local storage back to the page so that it will remain even after refresh
-$('.time-block').each(function() {
-  var key = $(this).attr('id');
-  var value = localStorage.getItem(key);
-  $(this).children('.description').val(value);
-});
-
-// Call to functions
-    textInput();
+  // Set time blocks to corresponding color based on current time
+  function updateColor() {
+    $(".time-block").each(function () {
+      var timeHour = parseInt(this.id);
+      console.log(timeHour);
+      console.log(this);
+      if (timeHour === currentHour) {
+        $(this).toggleClass("present");
+      } else if (timeHour < currentHour) {
+        $(this).toggleClass("past");
+      } else {
+        $(this).toggleClass("future");
+      }
+    });
+  }
+  // Update time on page every minute
+  setInterval(function () {
     updateColor();
-   }  
-);
+  }, 60000);
+
+  // Grab input from text area and save to local storage when clicking save button
+  function textInput() {
+    $(".saveBtn").on("click", function () {
+      var key = $(this).parent().attr("id");
+      var value = $(this).siblings(".description").val();
+      localStorage.setItem(key, value);
+    });
+  }
+
+  // Return the information from local storage back to the page so that it will remain even after refresh
+  $(".time-block").each(function () {
+    var key = $(this).attr("id");
+    var value = localStorage.getItem(key);
+    $(this).children(".description").val(value);
+  });
+
+  // Call to functions
+  textInput();
+  updateColor();
+});
