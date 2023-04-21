690f134463da1d939e2b8f0c81cb00cff4c931a3
Brooke Adlesperger-Jones <badles@live.com>
2023-04-20 20:06:22 -0500

    added comments to script.js

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,9 @@
 $(function () {
+  // Set current date/time in military format
 var currentHour = dayjs().format('HH');
 $("#currentDay").text(dayjs().format('dddd, MMMM D, YYYY'));
 
+// Set time blocks to corresponding color based on current time
 function updateColor() {
   $('.time-block').each(function() {
     var timeHour = parseInt(this.id);
@@ -16,11 +18,12 @@ function updateColor() {
     }
   });
 }
-
+// Update time on page every minute
 setInterval(function() {
   updateColor();
 }, 60000);
 
+// Grab input from text area and save to local storage when clicking save button 
 function textInput() {
   $('.saveBtn').on('click', function() {
     var key = $(this).parent().attr('id');
@@ -28,12 +31,15 @@ function textInput() {
     localStorage.setItem(key, value);
   });
 }
+
+// Return the information from local storage back to the page so that it will remain even after refresh
 $('.time-block').each(function() {
   var key = $(this).attr('id');
   var value = localStorage.getItem(key);
   $(this).children('.description').val(value);
 });
 
+// Call to functions
     textInput();
     updateColor();
    }  
